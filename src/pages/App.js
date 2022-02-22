ac9131f2285aa43cbce2daf33b6cb773ca616f03
Lucas <lavendanozarza@alu.frlp.utn.edu.ar>
2022-02-22 00:59:06 -0300

    commit added lazy load

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,37 +1,52 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "../styles/App.css";
-import Login from "./Login";
-import Home from "./Home";
 import NavBar from "../components/NavBar";
-import AddItem from "./AddItem";
 import useToken from "../hooks/useToken";
 import NotFound from "./NotFound";
+import { lazy, Suspense } from "react";
+import { Spinner } from "reactstrap";
 
-// TODO: HACER PAGINA 404
 // TODO: EMBELLECER UI
 // TODO: LEER SOBRE LAZY LOADING (LA SECCION DE USO EN RUTAS: https://es.reactjs.org/docs/code-splitting.html#route-based-code-splitting)
-// TODO: AGREGAR TESTS UNITARIOS
+
+const Home = lazy(() => import("./Home"));
+const AddItem = lazy(() => import("./AddItem"));
+const Login = lazy(() => import("./Login"));
 
 function App() {
   const { token, setToken } = useToken();
 
   if (!token) {
     return (
-      <>
-        <h1>Alkemy React Challenge</h1>
+      <Suspense
+        fallback={
+          <div className="text-center">
+            <Spinner color="info" size="sm" /> Cargando...
+          </div>
+        }
+      >
+        <h1 className="text-center">Alkemy React Challenge</h1>
         <Login token={token} setToken={setToken} />
-      </>
+      </Suspense>
     );
   }
 
   return (
     <BrowserRouter>
       <NavBar token={token} setToken={setToken} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add-item" element={<AddItem />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="text-center">
+            <Spinner color="info" size="sm" /> Cargando...
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-item" element={<AddItem />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
